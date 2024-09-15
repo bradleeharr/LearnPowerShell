750bf148332d1d7b54d75f23ce3eee3409739643
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-09-14 23:59:45 -0500

    Basic Commands 1

---
+++ Chapter 1/basic-commands-note.ps1
@@ -0,0 +1,12 @@
+# CMD       # DESCRIPTION
+Dir         # Listing of files and folders from the current folder
+Copy        # Copy a file to a different Location
+Cd          # Change Directories
+Move        # Move a file to a new location
+Ren         # Rename an existing file
+MkDir       # Create a new Directory
+Del         # Delete a File
+Type        # Dispaly the contents of a text file
+
+# Pipe Command
+Dir | More  # Paged - List all Directories
